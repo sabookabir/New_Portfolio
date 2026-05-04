53d9188359386e4da3110798d133cdf47291dbe2
Sabookabir <Sabookabir@gmail.com>
2026-05-04 18:19:08 +0530

    prepping for render deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
@@ -12,15 +13,25 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// API Routes
 app.use('/api/projects', require('./routes/projectRoutes'));
 app.use('/api/skills', require('./routes/skillRoutes'));
 app.use('/api/messages', require('./routes/messageRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
 
-app.get('/', (req, res) => {
-    res.send('Portfolio API is running...');
-});
+// Serve Frontend in Production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('Portfolio API is running...');
+    });
+}
 
 // Database Connection
 mongoose.connect(process.env.MONGODB_URI)
